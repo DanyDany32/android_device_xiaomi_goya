39bfb0a935b2094a4163bbd470ab8c97e79601b0
AdarshGrewal <adarshgrewal@gmail.com>
2026-03-31 01:40:39 +0530

    goya: sepolicy: Allow parts to use displayfeature
    Change-Id: I7b8a8ed50da73baebe1561df6dddd00ddc3f4044

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,7 +150,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
