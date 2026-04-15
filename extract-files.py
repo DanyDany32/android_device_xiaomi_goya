defd35590677c7f1993efe1a908459d44a9fd8aa
Mashopy <eliasgheeraert@gmail.com>
2026-04-15 22:35:11 +0200

    goya: Initial proprietary files and firmware listing

---
+++ extract-files.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/goya',
+    'hardware/mediatek',
+    'hardware/xiaomi',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'libneuron_graph_delegate.mtk',
+        'libnir_neon_driver_ndk.mtk.vndk',
+        'libtflite_mtk',
+        'vendor.mediatek.hardware.apuware.apusys-V5-ndk',
+        'vendor.mediatek.hardware.apuware.utils-V1-ndk',
+        'vendor.mediatek.hardware.apuware.utils@2.0',
+        'vendor.mediatek.hardware.neuropilot.agent-V1-ndk',
+        'vendor.mediatek.hardware.videotelephony-V1-ndk'
+    ): lib_fixup_vendor_suffix,
+}
+
+
+blob_fixups: blob_fixups_user_type = {
+    (
+        'odm/bin/hw/vendor.xiaomi.hw.touchfeature-service',
+        'odm/lib64/libadaptivehdr.so',
+        'odm/lib64/libcolortempmode.so',
+        'odm/lib64/libdither.so',
+        'odm/lib64/libflatmode.so',
+        'odm/lib64/libhistprocess.so',
+        'odm/lib64/libmiBrightness.so',
+        'odm/lib64/libmiSensorCtrl.so',
+        'odm/lib64/libpaperMode.so',
+        'odm/lib64/librhytheyecare.so',
+        'odm/lib64/libsdr2hdr.so',
+        'odm/lib64/libsre.so',
+        'odm/lib64/libtruetone.so',
+        'odm/lib64/libvideomode.so',
+        'vendor/bin/mnld',
+        'vendor/lib64/mt6899/libaalservice.so',
+        'vendor/lib64/mt6899/libpqconfig.so'
+    ): blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
+    (
+        'odm/bin/hw/vendor.xiaomi.sensor.citsensorservice.aidl',
+        'odm/lib64/hw/displayfeature.default.so'
+    ): blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    (
+        'odm/lib64/camera/plugins/capture/com.xiaomi.plugin.gainmap.so',
+        'odm/lib64/camera/plugins/capture/com.xiaomi.plugin.jpegrAggr.so',
+    ): blob_fixup()
+        .replace_needed('libultrahdr.so', 'libultrahdr-v35.so'),
+    (
+        'odm/lib64/libAncHumanPreviewBokeh.so',
+        'odm/lib64/libMiEmojiEffect.so',
+        'odm/lib64/libMiVideoFilter.so',
+        'odm/lib64/libTrueSight.so',
+        'odm/lib64/libwa_widelens_undistort.so',
+        'vendor/lib64/mt6899/libneuralnetworks_sl_driver_mtk_prebuilt.so',
+        'vendor/lib64/mt6899/libneuron_adapter_mgvi.so',
+        'vendor/lib64/libMiPhotoFilter.so',
+        'vendor/lib64/libmcve.so'
+    ): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_createFromHandle')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_isSupported')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_lockPlanes')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    (
+        'odm/lib64/libmiXmlParser.so',
+        'vendor/lib64/hw/audio.primary.mediatek.so',
+        'vendor/lib64/hw/mt6899/vendor.mediatek.hardware.pq_aidl-impl.so',
+        'vendor/lib64/libHardwareBacklightcore.so',
+        'vendor/lib64/lib_power_applist.so',
+        'vendor/lib64/libaudiocloudctrl.so',
+        'vendor/lib64/libmicamera_aidl_provider.so',
+        'vendor/lib64/libmicamera_hal_core.so',
+        'vendor/lib64/libpowerhal.so',
+        'vendor/lib64/libpqxmlflagparser.so',
+        'vendor/lib64/libpqxmlparser.so',
+        'vendor/lib64/librt_extamp_intf.so',
+        'vendor/lib64/libsilkybrightnesscore.so',
+        'vendor/lib64/libxlog.so',
+        'vendor/lib64/mt6899/lib3a.custom.ae.flow.so',
+        'vendor/lib64/mt6899/libmmlpqImpl.so'
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    (
+        'odm/lib64/libmt_mitee.so',
+        'odm/lib64/libgoogleid.so',
+    ): blob_fixup()
+        .replace_needed('lib_android_keymaster_keymint_utils.so', 'lib_android_keymaster_keymint_utils_V3.so')
+        .replace_needed('libkeymint.so', 'libkeymint_V3.so')
+        .replace_needed('libkeymint_remote_prov_support.so', 'libkeymint_remote_prov_support_V3.so')
+        .replace_needed('libkeymint_support.so', 'libkeymint_support_V3.so'),
+    'system_ext/bin/hw/android.hardware.audio.parameter_parser.service': blob_fixup()
+        .replace_needed('av-audio-types-aidl-ndk.so', 'av-audio-types-aidl-V3-ndk.so'),
+    'system_ext/lib64/libimsma.so': blob_fixup()
+        .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
+        .apktool_patch('blob-patches/ImsService'),
+    'vendor/bin/hw/android.hardware.audio.service-aidl.mediatek': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V5-ndk.so', 'android.media.audio.common.types-V3-ndk.so')
+        .replace_needed('libaudio_aidl_conversion_common_ndk.so', 'libaudio_aidl_conversion_common_ndk_prebuilt.so'),
+    (
+        'vendor/bin/hw/mt6899/android.hardware.graphics.allocator-V2-service-mediatek.mt6899',
+        'vendor/lib64/egl/mt6899/libGLES_mali.so',
+        'vendor/lib64/hw/mt6899/android.hardware.graphics.allocator-V2-mediatek.so',
+        'vendor/lib64/hw/mt6899/mapper.mediatek.so',
+        'vendor/lib64/mt6899/libmtkcam_grallocutils.so',
+        'vendor/lib64/libcodec2_fsr.so',
+        'vendor/lib64/libcodec2_vpp_AIMEMC_plugin.so',
+        'vendor/lib64/libcodec2_vpp_AISR_plugin.so',
+        'vendor/lib64/libgpud.so',
+        'vendor/lib64/libmtkcam_grallocutils_aidlv2helper.so',
+        'vendor/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so',
+        'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V2-ndk.so',
+        'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V4-ndk.so',
+        'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V7-ndk.so'
+    ): blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
+    'vendor/bin/hw/android.hardware.security.keymint@3.0-service.mitee': blob_fixup()
+        .replace_needed('lib_android_keymaster_keymint_utils.so', 'lib_android_keymaster_keymint_utils_V3.so')
+        .replace_needed('libkeymint.so', 'libkeymint_V3.so'),
+    'vendor/etc/vintf/manifest/manifest_media_c2_default.xml': blob_fixup()
+        .regex_replace('    <fqname>IComponentStore/dolby</fqname>\n', ''),
+    'vendor/lib64/hw/android.hardware.audio.effect.aidl-impl-mediatek.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V5-ndk.so', 'android.media.audio.common.types-V3-ndk.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    'vendor/lib64/hw/android.hardware.soundtrigger3-impl.so': blob_fixup()
+        .replace_needed('libaudio_aidl_conversion_common_ndk.so', 'libaudio_aidl_conversion_common_ndk_prebuilt.so'),
+    'vendor/lib64/hw/hwcomposer.mtk_common.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    (
+        'vendor/lib64/hw/hwcomposer.mtk_common.so',
+        'vendor/lib64/mt6899/libmtkcam_taskmgr.so',
+        'vendor/lib64/libcameraopt.so'
+    ): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
+    (
+        'vendor/lib64/soundfx/libaecsw_mtk.so',
+        'vendor/lib64/soundfx/libagc1sw_mtk.so',
+        'vendor/lib64/soundfx/libagc2sw_mtk.so',
+        'vendor/lib64/soundfx/libnssw_mtk.so',
+        'vendor/lib64/soundfx/libpreprocessingaidl_mtk.so',
+    ): blob_fixup()
+        .replace_needed('android.media.audio.common.types-V5-ndk.so', 'android.media.audio.common.types-V3-ndk.so'),
+    'vendor/lib64/android.hardware.audio.core-impl-mediatek.so': blob_fixup()
+        .add_needed('libaudioutils_shim.so')
+        .replace_needed('android.media.audio.common.types-V5-ndk.so', 'android.media.audio.common.types-V3-ndk.so')
+        .replace_needed('libaudio_aidl_conversion_common_ndk.so', 'libaudio_aidl_conversion_common_ndk_prebuilt.so'),
+    'vendor/lib64/libaudio_aidl_conversion_common_ndk_prebuilt.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V5-ndk.so', 'android.media.audio.common.types-V3-ndk.so'),
+    'vendor/lib64/libkeymint_V3.so': blob_fixup()
+        .replace_needed('lib_android_keymaster_keymint_utils.so', 'lib_android_keymaster_keymint_utils_V3.so'),
+    'vendor/lib64/libkeymint_remote_prov_support_V3.so': blob_fixup()
+        .replace_needed('libbase.so', 'libbase-v35.so'),
+    'vendor/lib64/libultrahdr-v35.so': blob_fixup()
+        .replace_needed('libjpegdecoder.so', 'libjpegdecoder-v35.so')
+        .replace_needed('libjpegencoder.so', 'libjpegencoder-v35.so'),
+    'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V7-ndk.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
+    (
+        'vendor/lib64/vendor.xiaomi.hardware.camera.injection-V1-ndk.so',
+        'vendor/lib64/vendor.xiaomi.hardware.camera.injection-client.so',
+        'vendor/lib64/vendor.xiaomi.hardware.camera.injection-service.so'
+    ): blob_fixup()
+        .replace_needed('android.hardware.camera.device-V1-ndk.so', 'android.hardware.camera.device-V2-ndk.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'goya',
+    'xiaomi',
+    add_firmware_proprietary_file=True,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
