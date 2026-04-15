0836c759941c913ff354b8b5e6df8c18f3863359
Mashopy <eliasgheeraert@gmail.com>
2026-04-15 23:26:34 +0200

    goya: Inherit common MediaTek overlays

--- device.mk
+++ device.mk
@@ -255,6 +255,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
+# Overlays
+$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
+
 # Page size
 PRODUCT_NO_BIONIC_PAGE_SIZE_MACRO := true
 
