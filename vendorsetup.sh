85a1d74bb9218829a4c3f5d92801fbe801cc7939
DanyDany32 <46933185+DanyDany32@users.noreply.github.com>
2026-06-19 18:59:16 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
-# Scarica in automatico il kernel e i moduli pre-compilati
-if [ ! -d "device/xiaomi/goya-kernel" ]; then
-    git clone https://github.com/xiaomi-goya-devs/android_device_xiaomi_goya-kernel device/xiaomi/goya-kernel
-fi
+echo "Scaricando il Kernel e i Moduli proprietari..."
+mkdir -p device/xiaomi/goya-kernel
+wget -O kernel.zip "https://github.com/DanyDany32/android_device_xiaomi_goya/releases/download/v1.0/goya_kernel.zip"
+unzip -o kernel.zip -d device/xiaomi/goya-kernel/
+rm kernel.zip
