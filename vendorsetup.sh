ba41eeb8e445020da468218cfeec3fc72f47c834
DanyDany32 <46933185+DanyDany32@users.noreply.github.com>
2026-06-19 22:19:09 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,7 @@ mkdir -p device/xiaomi/goya-kernel
 wget -O kernel.zip "https://github.com/DanyDany32/android_device_xiaomi_goya/releases/download/v1.0/goya_kernel.zip"
 unzip -o kernel.zip -d device/xiaomi/goya-kernel/
 rm kernel.zip
+
+# IL FIX DEL DTB: Spostiamo il file in una cartella e aggiungiamo l'estensione .dtb!
+mkdir -p device/xiaomi/goya-kernel/dtb_dir
+mv device/xiaomi/goya-kernel/dtb device/xiaomi/goya-kernel/dtb_dir/goya.dtb
