8e184756186ad09466dc706428b3cff9d793ae0d
DanyDany32 <46933185+DanyDany32@users.noreply.github.com>
2026-06-20 17:18:49 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,4 @@ mv device/xiaomi/goya-kernel/dtb device/xiaomi/goya-kernel/dtb_dir/goya.dtb
 export OF_SKIP_FBE_DECRYPTION=1
 export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 export OF_SKIP_DECRYPTED_DATA=1
+export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
