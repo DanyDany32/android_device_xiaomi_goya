a553d6abb7a73b822f3d3944395c4b1309aeb11d
DanyDany32 <46933185+DanyDany32@users.noreply.github.com>
2026-06-21 14:01:15 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,3 +13,16 @@ export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 export OF_SKIP_DECRYPTED_DATA=1
 export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
 export OF_NO_SPLASH_STAY=1
+
+# === FIX GRAFICO E SCHERMO ===
+# Disabilita l'animazione di avvio che fa crashare il DRM su MediaTek
+export OF_DISABLE_BOOT_ANIMATION=1
+
+# Diciamo alla recovery le dimensioni esatte per non farla impazzire
+export OF_SCREEN_H=2712
+export OF_STATUS_H=120
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+
+# Evita loop dovuti a problemi col touch
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
